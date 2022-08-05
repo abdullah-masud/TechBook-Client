d2db4c209b4917a423d474d571c9d06c7b766e58
Abdullah Al Masud <masudctg00@gmail.com>
2022-08-06 02:00:02 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home'
+import Blogs from './Pages/Blogs/Blogs';
+import Write from './Pages/Write/Write';
+import About from './Pages/About/About';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='blogs' element={<Blogs />} />
+        <Route path='write' element={<Write />} />
+        <Route path='about' element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Write/Write.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Write = () => {
+    return (
+        <div>
+            <h2>User can write blog</h2>
+        </div>
+    );
+};
+
+export default Write;
