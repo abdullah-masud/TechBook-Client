9693d2471dc7641e55ee79e1dcfcf4ed0468e3b6
Abdullah Al Masud <masudctg00@gmail.com>
2022-08-12 11:35:53 +0600

    filter by category done

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,18 +1,29 @@
-import React from 'react';
 import useBlogs from '../../hooks/useBlogs';
 import Blog from '../Home/HomeBlogs/Blog';
 
 const Blogs = () => {
-    const [blogs] = useBlogs()
+    const [blogs, setBlogs] = useBlogs()
 
-    const filteredBlogs = blogs?.filter(blog => blog.category === 'food');
-    console.log(filteredBlogs)
+    const getFilteredBlogs = (category) => {
+        const blogCategory = category
+        const url = `http://localhost:5000/categoryblogs?category=${blogCategory}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }
+
+    const getAllBlogs = () => {
+        const url = `http://localhost:5000/allblogs`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }
 
     return (
         <div className='flex lg:flex-row flex-col-reverse min-h-screen max-w-7xl mx-auto my-12 justify-center'>
             <div className='grid px-3'>
                 {
-                    blogs.map(blog => <Blog
+                    blogs?.map(blog => <Blog
                         key={blog._id}
                         blog={blog}
                     />)
@@ -20,10 +31,10 @@ const Blogs = () => {
             </div>
             <div className="lg:divider lg:divider-horizontal"></div>
             <div className='flex flex-row lg:justify-start justify-center lg:flex-col gap-3 lg:mt-4'>
-                <button className='btn btn-xs btn-outline btn-primary'>All</button>
-                <button className='btn btn-xs btn-outline btn-primary'>Programming</button>
-                <button className='btn btn-xs btn-outline btn-primary'>Food</button>
-                <button className='btn btn-xs btn-outline btn-primary'>Travel</button>
+                <button onClick={getAllBlogs} className='btn btn-xs btn-outline btn-primary'>All</button>
+                <button onClick={() => getFilteredBlogs('programming')} className='btn btn-xs btn-outline btn-primary'>Programming</button>
+                <button onClick={() => getFilteredBlogs('food')} className='btn btn-xs btn-outline btn-primary'>Food</button>
+                <button onClick={() => getFilteredBlogs('travel')} className='btn btn-xs btn-outline btn-primary'>Travel</button>
             </div>
         </div>
     );
--- src/hooks/useBlogs.js
+++ src/hooks/useBlogs.js
@@ -1,15 +1,19 @@
+import axios from "axios"
 import { useEffect, useState } from "react"
 
 const useBlogs = () => {
     const [blogs, setBlogs] = useState([])
 
     useEffect(() => {
-        fetch('http://localhost:5000/allblogs')
-            .then(res => res.json())
-            .then(data => setBlogs(data))
+        axios.get('http://localhost:5000/allblogs')
+            .then(data => setBlogs(data.data))
     }, [])
 
     return [blogs, setBlogs];
 }
 
 export default useBlogs
+
+/* fetch('http://localhost:5000/allblogs')
+            .then(res => res.json())
+            .then(data => setBlogs(data)) */
