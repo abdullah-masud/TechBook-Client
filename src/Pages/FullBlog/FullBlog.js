35e99bd4c89fbb7dc9f85399dad00e3c450bc7b2
Abdullah Al Masud <masudctg00@gmail.com>
2022-08-23 12:23:12 +0600

    full blog page designed

--- src/Pages/FullBlog/FullBlog.js
+++ src/Pages/FullBlog/FullBlog.js
@@ -15,10 +15,14 @@ const FullBlog = () => {
 
 
     return (
-        <div className='min-h-screen max-w-7xl mx-auto mt-4 mb-12 px-2'>
-            <img className='w-full ' src={image} alt="" />
-            <h1 className='lg:text-4xl text-center text-2xl my-4 font-semibold'>{title}</h1>
-            <p className='lg:text-lg text-justify px-2'> {description}</p>
+        <div className='max-w-7xl mx-auto mt-4 mb-12 px-3'>
+            <div className='lg:my-20'>
+                <img className='lg:float-left lg:pr-4 lg:pl-2 lg:w-2/4' src={image} alt="" />
+                <div className=''>
+                    <h1 className='lg:text-4xl lg:text-left text-center text-2xl lg:mb-4 lg:my-0 my-4 font-semibold'>{title}</h1>
+                    <p className='lg:text-lg text-justify lg:px-2'> {description}</p>
+                </div>
+            </div>
         </div>
     );
 };
